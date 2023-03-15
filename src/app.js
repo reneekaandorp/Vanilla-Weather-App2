32056a6063d1503b1c3941b09d916f78959e22de
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2023-03-15 12:13:42 +0100

    Updated date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0{hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -9,11 +34,13 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   console.log(response);
 }
 
 let apiKey = "79t19ca06b3618febf143dc04f0o86be";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=79t19ca06b3618febf143dc04f0o86be&units=metric";
+  "https://api.shecodes.io/weather/v1/current?query=Diever&key=79t19ca06b3618febf143dc04f0o86be&units=metric";
 
 axios.get(apiUrl).then(displayTemperature);
