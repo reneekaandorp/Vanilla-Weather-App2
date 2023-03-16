f3b4c7fa1d9241f524f4c4207b60c07a5dfa4a80
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2023-03-16 15:54:26 +0100

    Worked on function displayForecast

--- src/app.js
+++ src/app.js
@@ -23,6 +23,38 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                <div class="col-2">
+                  <div class="weather-forecast-date">
+                    ${day}
+                    <img
+                      src="https://openweathermap.org/img/wn/50d@2x.png"
+                      alt=""
+                      width="42"
+                    />
+                    <div class="weather-forecast-temperatures">
+                      <span class="weather-forecast-temperature-max">6°</span>
+                      <span class="weather-forecast-temperature-min">4°</span>
+                    </div>
+                  </div>
+                </div>
+              `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -83,3 +115,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Diever");
+displayForecast();
